ec085183a83f5ae68b1b90bd38320dc491358d48
filipe <filipedasilva94.1@gmail.com>
2021-10-19 23:25:38 -0300

    slug automático em create view

--- meu_site/blog/models.py
+++ meu_site/blog/models.py
@@ -1,8 +1,12 @@
-from django.db import models
+from django.db                       import models
+from django.dispatch                 import receiver                
 from django.db.models.fields.related import create_many_to_many_intermediary_model
-from django.utils import timezone
-from django.contrib.auth.models import User
-from django.urls import reverse
+from django.db.models.signals        import post_save
+from django.utils                    import timezone
+from django.urls                     import reverse
+from django.utils.text               import slugify
+from django.contrib.auth.models      import User
+
 
 # Create your models here.
 
@@ -32,13 +36,19 @@ class Post(models.Model):
     published = PublishedManager()
 
     def get_absolute_url(self):
-        return reverse('post_detail',args=[self.slug])
+        return reverse('post_detail',args=[self.pk])
 
     def get_absolute_url_update(self):
-        return reverse('post_edit',args=[self.slug])
+        return reverse('post_edit',args=[self.pk])
 
     class Meta:
         ordering = ('-publicado',)
 
     def __str__(self):
         return self.titulo
+
+@receiver(post_save,sender=Post)
+def insert_slug(sender,instance,**kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.titulo)
+        return instance.save()
